3d8fd59a4803845d401f0a3c32bc17d57a487dff
Eren <dev.erenesen@gmail.com>
2025-03-27 19:23:25 +0300

    Swagger çalışıyor

---
+++ OnlineKutuphane.API/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetBooks()
+    {
+        return Ok(new List<string> { "Kitap 1", "Kitap 2" });
+    }
+}
